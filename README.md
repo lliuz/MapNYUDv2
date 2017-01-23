9f970f6b1c34bc2c50bf5a0cea8570e169239286
lliuz <melpancake@gmail.com>
2017-01-24 01:57:24 +0800

    ...

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MapNYUDv2
-mapping the origin 800+ classes to 40 classes
+Mapping the origin 800+ classes to 40 classes. The raw data and the map of classes can be find in original fcn project.
---
+++ map40classes.m
@@ -0,0 +1,23 @@
+addpath('./matlab_utils')
+
+load('benchmarkData/metadata/classMapping40.mat')
+
+label_dir = 'benchmarkData/groundTruth';
+output_dir = 'benchmarkData/groundTruth40cls';
+mkdir_notexist(output_dir);
+
+mapClass = [255, mapClass];
+mask_cmap = labelcolormap(256);
+raw_label_files = my_list_file(label_dir)';
+
+for i = 1:length(raw_label_files)
+    file_name = raw_label_files{i};
+    file_path = fullfile(label_dir, raw_label_files{i});
+    one_file = load(file_path);
+    raw_label = one_file.groundTruth{1}.Segmentation + 1;
+    map_label = mapClass(uint16(raw_label));
+    assert( all(map_label(:) <= 255) && all(map_label(:) >= 0));
+    
+    output_path = fullfile(output_dir,  strrep(file_name,'.mat', '.png'));
+    imwrite(uint8(map_label), mask_cmap, output_path);
+end
